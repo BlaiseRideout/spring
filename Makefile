9c2101a0fff55e8838ee291d95458d42fbc038e7
John Anthony <johnanthony@lavabit.com>
2011-10-13 23:21:17 +0100

    Initial window

--- Makefile
+++ Makefile
@@ -1,12 +1,13 @@
 CC			= gcc
 CPPFLAGS 	:= -Wall --std=c99 --pedantic -O2
 GTKCFLAGS	= `pkg-config --cflags gtk+-2.0`
+GTKLIBS 	= `pkg-config --libs gtk+-2.0`
 BINNAME		= spring
 
 .PHONY: clean install
 
 $(BINNAME): $(BINNAME).c
-	$(CC) $(CPPFLAGS) $(GTKCFLAGS) -o $@ $<
+	$(CC) $(CPPFLAGS) $(GTKCFLAGS) $(GTKLIBS) -o $@ $<
 	
 clean:
 	rm -vf $(BINNAME)
--- spring.c
+++ spring.c
@@ -3,7 +3,19 @@
 #include <unistd.h>
 #include <stdio.h>
 
+static GtkWidget *window;
+static GtkWidget *textEntry;
+
 int main(int argc, char **argv) {
+    gtk_init(&argc, &argv);
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_widget_set_size_request(window, 800, 30);
+    gtk_container_set_border_width(GTK_CONTAINER(window), 0);
+
+    gtk_widget_show_all(window);
+
+    gtk_main();
 
     return 0;
 }
