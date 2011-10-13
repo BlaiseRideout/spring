36dfcebd0373ba9ba71f393c873b6f5f428a2f5a
John Anthony <johnanthony@lavabit.com>
2011-10-14 00:01:30 +0100

    Preliminary text entry

--- spring.c
+++ spring.c
@@ -4,12 +4,19 @@
 #include <stdio.h>
 
 static GtkWidget *window;
-static GtkWidget *textEntry;
+static GtkWidget *textbox;
 
-static gboolean delete(GtkWidget *widget, GtkWidget *ev, gpointer data);
+static gboolean handle_keypress(GtkWidget *widget, GtkWidget *ev, gpointer data);
+static gboolean killevent(GtkWidget *widget, GtkWidget *ev, gpointer data);
 
 static gboolean
-delete(GtkWidget *widget, GtkWidget *ev, gpointer data) {
+handle_keypress(GtkWidget *widget, GtkWidget *ev, gpointer data) {
+
+    return FALSE;
+}
+
+static gboolean
+killevent(GtkWidget *widget, GtkWidget *ev, gpointer data) {
     gtk_main_quit ();
     return FALSE;
 }
@@ -17,14 +24,21 @@ delete(GtkWidget *widget, GtkWidget *ev, gpointer data) {
 int main(int argc, char **argv) {
     gtk_init(&argc, &argv);
 
+    /* Window widget */
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_widget_set_size_request(window, 800, 30);
     gtk_container_set_border_width(GTK_CONTAINER(window), 0);
     gtk_window_set_policy(GTK_WINDOW(window), FALSE, FALSE, FALSE);
 
-    g_signal_connect(window, "delete-event", G_CALLBACK(delete), NULL);
-    /* g_signal_connect(window, "key-press-event", G_CALLBACK(handle_keypress), NULL); */
+    /* Text entry widget */
+    textbox = gtk_entry_new();
+    gtk_container_add(GTK_CONTAINER (window), textbox);
 
+    /* Signal handling */
+    g_signal_connect(window, "delete-event", G_CALLBACK(killevent), NULL);
+    g_signal_connect(window, "key-press-event", G_CALLBACK(handle_keypress), NULL);
+
+    gtk_widget_show(textbox);
     gtk_widget_show_all(window);
 
     gtk_main();
